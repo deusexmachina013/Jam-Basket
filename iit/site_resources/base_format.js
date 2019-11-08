1fe046e4936f05c9ab9668892a6ee579edfd9300
Jacob Dyer <dyerj@rpi.edu>
2019-11-08 17:51:23 -0500

    Added Header and Footer
    Added the header and footer parts of the base_format script.

--- iit/site_resources/base_format.js
+++ iit/site_resources/base_format.js
@@ -1,33 +1,45 @@
 // This script loads in the header, the menu bar, and the footer of the site.
 
 $(document).ready(function() {
-    // Write the header.
-    var headerHTML = "";
-    
-    headerHTML += "<section id='header'>";
-    headerHTML += "</section>";
-    
-    // Load in the header.
-    $("section #content").before(headerHTML);
-    
-    // Write the menu bar.
-    var menuBarHTML = "";
-    
-    menuBarHTML += "<section id='menu'>"
-    menuBarHTML += "<ul class='bar'>"
-    menuBarHTML += "<li>Home</li>"
-    menuBarHTML += "<li>Browse</li>"
-    menuBarHTML += "<li>Community</li>"
-    menuBarHTML += "<li>"
-    menuBarHTML += "<form>"
-    menuBarHTML += "<label for='search'>"
-    menuBarHTML += "<input type='text' size='20' placeholder='Search'>"
-    menuBarHTML += "</label>"
-    menuBarHTML += "</form>"
-    menuBarHTML += "</li>"
-    menuBarHTML += "</ul>"
-    menuBarHTML += "</section>"
-    
-    // Load in the menu bar.
-    $("#content").before(menuBarHTML);
+  // Write the header.
+  var headerHTML = "";
+  
+  headerHTML += "<section id='header'>";
+  headerHTML += "<hl>Jam Basket</h1>";
+  headerHTML += "</section>";
+  
+  // Load in the header.
+  $("#content").before(headerHTML);
+  
+  // Write the menu bar.
+  var menuBarHTML = "";
+  
+  menuBarHTML += "<section id='menu'>"
+  menuBarHTML += "<ul class='bar'>"
+  menuBarHTML += "<li>Home</li>"
+  menuBarHTML += "<li>Browse</li>"
+  menuBarHTML += "<li>Community</li>"
+  menuBarHTML += "<li>"
+  menuBarHTML += "<form>"
+  menuBarHTML += "<label for='search'>"
+  menuBarHTML += "<input type='text' size='20' placeholder='Search'>"
+  menuBarHTML += "</label>"
+  menuBarHTML += "</form>"
+  menuBarHTML += "</li>"
+  menuBarHTML += "</ul>"
+  menuBarHTML += "</section>"
+  
+  // Load in the menu bar.
+  $("#content").before(menuBarHTML);
+  
+  // Write the footer.
+  var footerHTML = "";
+  
+  footerHTML += "<section id='footer'>";
+  footerHTML += "<footer>";
+  footerHTML += "<p>This is a student project by Jacob Dyer, Anthony Bonadies, and Tyler Green.</p>";
+  footerHTML += "</footer>";
+  footerHTML += "</section>";
+  
+  $("#content").after(footerHTML);
 });
