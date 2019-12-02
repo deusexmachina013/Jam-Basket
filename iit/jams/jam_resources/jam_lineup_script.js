8ff5f67d7ac0110eb6add20b203e55f720557f18
Jacob Dyer <dyerj@rpi.edu>
2019-12-02 16:19:44 -0500

    Moved Stuff Around
    Little to explain.

--- iit/jams/jam_resources/jam_lineup_script.js
+++ iit/jams/jam_resources/jam_lineup_script.js
@@ -1,39 +1,12 @@
 $(document).ready(function() {
     
-    // Allow the user to toggle the accordions.
-    $(".accordion").click(function() {
-        $(this).toggleClass("active");
-        
-        var panel = this.nextElementSibling;
-        if(panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-    
-    // Allow the user to filter jams by name or theme.
-    $("#filterBar").on("keyup", function() {
-        var filter = this.value.toUpperCase();
-        var list = $("li");
-        
-        for(index = 0; index < list.length; index++) {
-            button = list[index].getElementsByTagName("button")[0];
-            content = button.textContent || button.innerText;
-            
-            if(content.toUpperCase().indexOf(filter) > -1) {
-                list[index].style.display = "";
-            } else {
-                list[index].style.display = "none";
-            }
-        }
-    });
+    // Load the Jams //
     
     // Attempt to populate the jam lineup page with information found in the JSON file.
     $.ajax({
         // Put out a GET request for the requisite JSON file.
         type: "GET",
-        url: "mock_jams.json",
+        url: "jam_resources/mock_jams.json",
         dataType: "json",
 
         // If the get request is successful, scrape the JSON data and format it for the webpage.
@@ -64,5 +37,36 @@ $(document).ready(function() {
         error: function(msg) {
           alert("This page is unable to read the jams file.");
         }
-      });
+    });
+    
+    // Site Interactions //
+    
+    // Allow the user to toggle the accordions.
+    $(".accordion").click(function() {
+        $(this).toggleClass("active");
+        
+        var panel = this.nextElementSibling;
+        if(panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+    
+    // Allow the user to filter jams by name or theme.
+    $("#filterBar").on("keyup", function() {
+        var filter = this.value.toUpperCase();
+        var list = $("li");
+        
+        for(index = 0; index < list.length; index++) {
+            button = list[index].getElementsByTagName("button")[0];
+            content = button.textContent || button.innerText;
+            
+            if(content.toUpperCase().indexOf(filter) > -1) {
+                list[index].style.display = "";
+            } else {
+                list[index].style.display = "none";
+            }
+        }
+    });
 });
